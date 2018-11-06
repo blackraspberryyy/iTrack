c46cc3970c6b5b06654b127e0f79a69f0bdb5e73
Juan Carlo Valencia <carlo.valencia066@gmail.com>
2018-11-07 00:56:07 +0800

    Add minor Reports (Not Finished)

---
+++ application/controllers/AdminMinorReports.php
@@ -0,0 +1,35 @@
+<?php
+
+class AdminMinorReports extends CI_Controller {
+
+  function __construct() {
+    parent::__construct();
+    if ($this->session->has_userdata('isloggedin') == FALSE) {
+      //user is not yet logged in
+      $this->session->set_flashdata("err_login", "Login First!");
+      redirect(base_url() . 'adminlogin/');
+    } else {
+      if($this->session->userdata("useraccess") == "student" || $this->session->userdata("useraccess") == "teacher"){
+        $this->session->set_flashdata("err_login", "Restricted Subpage");
+        redirect(base_url() . 'userdashboard/');
+      }else if($this->session->userdata("useraccess") == "admin"){
+        //Do nothing
+      }
+    }
+  }
+
+  public function index() {
+    $where = array(
+        "admin_id" => $this->session->userdata("userid")
+    );
+    
+    $data = array(
+        'title'         => "Minor Reports",
+        'currentadmin'  => $this->AdminDashboard_model->getAdmin($where)[0],
+        'cms'           => $this->AdminCMS_model->getCMS()[0]
+    );
+    $this->load->view("admin_includes/nav_header", $data);
+    $this->load->view("admin_minor_reports/main");
+    $this->load->view("admin_includes/footer");
+}
+}
---
+++ application/views/admin_minor_reports/main.php
@@ -0,0 +1,15 @@
+<div class="row">
+  <ol class="breadcrumb">
+    <li><a href="<?= base_url() ?>admindashboard">
+      <em class="fa fa-home"></em>
+    </a></li>
+    <li class="active">Minor Reports</li>
+  </ol>
+</div><!--/.row breadcrumb-->
+
+<div class="row">
+  <div class="col-xs-12">
+    <h1><?= $cms->minor_reports_title?></h1>
+    <h5><?= $cms->minor_reports_text?></h5>
+  </div>
+</div>
