7d9ea80598c1bd71da479add42367ceb5c76fc26
carlovalencia066 <carlo.valencia066@gmail.com>
2018-11-09 10:51:40 +0800

    fix Email

---
+++ application/controllers/email.php
@@ -0,0 +1,16 @@
+<?php
+
+/* $config['protocol'] = "smtp"; */
+/* $config['smtp_host'] = "ssl://mail.gmail.com";*/
+
+$config['protocol'] = 'mail';
+$config['smtp_host'] = "ssl://mail.gmail.com";
+$config['smtp_port'] = 465;
+$config['charset'] = "utf-8";
+$config['newline'] = "\r\n";
+$config['mailtype'] = "html";
+
+// Always use double quotes instead of single quotes for values
+$config['smtp_user'] = "itracksolutions.123@gmail.com";
+$config['smtp_pass'] = "Itracksolutions1";
+
