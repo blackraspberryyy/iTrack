3e424ae830464b550ed0d7b93b5a1b054d6fc4c1
Jefferson Rylee <rylee.jeff385@gmail.com>
2018-12-14 00:50:25 +0800

    Add notification helper

---
+++ application/helpers/notification_helper.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+if (!function_exists('notificate')) {
+  function notificate($id, $user_id, $user_token, $title, $body, $timestamp) {
+    $url = 'https://fcm.googleapis.com/fcm/send';
+    $API_KEY = fcm_keys('server');
+
+    $headers = array(
+      'Authorization: key=' . $API_KEY,
+      'Content-Type: application/json'
+    );
+
+    $fields = array(
+      'priority' => 'high',
+      'registration_ids' => array($user_token),
+      'message_id' => 'm-' . $id,
+      'notification' => array(
+        'title' => $title,
+        'body' => $body
+      ),
+      'data' => array(
+        'id' => $id,
+        'user_id' => $user_id,
+        'title' => $title,
+        'body' => $body,
+        'timestamp' => $timestamp
+      )
+    );
+
+    $fields = json_encode($fields);
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+
+    $result = curl_exec($ch);
+    echo $result;
+
+    curl_close($ch);
+  }
+}
