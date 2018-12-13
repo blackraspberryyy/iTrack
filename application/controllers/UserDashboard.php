1cf4202c849d2ab0a91bbf8e1a929e06ae6d9a16
Angelo Markus B. Zaguirre <32188141+abzaguirre@users.noreply.github.com>
2018-12-14 01:51:11 +0800

    Sanction Details

--- application/controllers/UserDashboard.php
+++ application/controllers/UserDashboard.php
@@ -1,42 +1,56 @@
 <?php
 
-class UserDashboard extends CI_Controller {
-
-    function __construct() {
+class UserDashboard extends CI_Controller
+{
+    public function __construct()
+    {
         parent::__construct();
-        if ($this->session->has_userdata('isloggedin') == FALSE) {
+        if ($this->session->has_userdata('isloggedin') == false) {
             //user is not yet logged in
-            $this->session->set_flashdata("err_login", "Login First!");
-            redirect(base_url() . 'Login/');
+            $this->session->set_flashdata('err_login', 'Login First!');
+            redirect(base_url().'Login/');
         } else {
-            if ($this->session->userdata("useraccess") == "student" || $this->session->userdata("useraccess") == "teacher") {
+            if ($this->session->userdata('useraccess') == 'student' || $this->session->userdata('useraccess') == 'teacher') {
                 //Do Nothing
-            } else if ($this->session->userdata("useraccess") == "admin") {
-                $this->session->set_flashdata("err_login", "Restricted Subpage");
-                redirect(base_url() . 'UserDashboard/');
+            } elseif ($this->session->userdata('useraccess') == 'admin') {
+                $this->session->set_flashdata('err_login', 'Restricted Subpage');
+                redirect(base_url().'UserDashboard/');
             }
         }
     }
 
-    public function index() {
-        $incidentReport = $this->UserIncidentReport_model->getIncidentReport(array('u.user_id' => $this->session->userdata("userid"), 'incident_report_status' => 1))[0];
-        $attendance = $this->UserDusap_model->getAttendance(array('att.incident_report_id' => $incidentReport->incident_report_id, 'incident_report_status' => 1));
-     
+    public function index()
+    {
+        $incidentReport = $this->UserIncidentReport_model->getIncidentReport(array('u.user_id' => $this->session->userdata('userid'), 'incident_report_status' => 1))[0];
+
         $where = array(
-            "user_id" => $this->session->userdata("userid")
+            'user_id' => $this->session->userdata('userid'),
         );
-        $data = array(
-            "title" => "Home",
-            'currentuser' => $this->UserDashboard_model->getUser($where)[0],
+        $currentUser = $this->UserDashboard_model->getUser($where)[0];
+        // echo '<pre>';
+        // print_r($currentUser);
+        // echo '</pre>';
+        // die;
+        if ($currentUser->user_access == 'student') {
+            $attendance = $this->UserDusap_model->getAttendance(array('att.incident_report_id' => $incidentReport->incident_report_id, 'incident_report_status' => 1));
+            $data = array(
+            'title' => 'Home',
+            'currentuser' => $currentUser,
             'cms' => $this->AdminCMS_model->getCMS()[0],
             'total_hours' => $this->AdminDusap_model->getAttendanceTotalHours(array('incident_report_id' => $incidentReport->incident_report_id))[0],
             'attendance' => $attendance,
             'incident_report' => $incidentReport,
         );
-
-        $this->load->view("user_includes/nav_header", $data);
-        $this->load->view("user_dashboard/main");
-        $this->load->view("user_includes/footer");
+        } else {
+            $data = array(
+                'title' => 'Home',
+                'currentuser' => $currentUser,
+                'cms' => $this->AdminCMS_model->getCMS()[0],
+                'incident_report' => $incidentReport,
+            );
+        }
+        $this->load->view('user_includes/nav_header', $data);
+        $this->load->view('user_dashboard/main');
+        $this->load->view('user_includes/footer');
     }
-
 }
